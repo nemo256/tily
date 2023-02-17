ab713934a5c2c3e39f78541e23529af7cf68b6d5
Amine Neggazi <neggazimedlamine@gmail.com>
2023-02-17 15:38:46 +0100

    removed unnecessary tags

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -248,11 +248,11 @@ static const Key keys[] = {
 	TAGKEYS(                  XK_2,                      1)
 	TAGKEYS(                  XK_3,                      2)
 	TAGKEYS(                  XK_4,                      3)
-	TAGKEYS(                  XK_5,                      4)
-	TAGKEYS(                  XK_6,                      5)
-	TAGKEYS(                  XK_7,                      6)
-	TAGKEYS(                  XK_8,                      7)
-	TAGKEYS(                  XK_9,                      8)
+	// TAGKEYS(                  XK_5,                      4)
+	// TAGKEYS(                  XK_6,                      5)
+	// TAGKEYS(                  XK_7,                      6)
+	// TAGKEYS(                  XK_8,                      7)
+	// TAGKEYS(                  XK_9,                      8)
 	{ MODKEY|ShiftMask,       XK_q,      quit,           {0} },
 };
 
