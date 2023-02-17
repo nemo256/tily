909fa76c5cba5582a787ea7d992d9752bfc77cc3
Amine Neggazi <neggazimedlamine@gmail.com>
2023-02-17 16:32:24 +0100

    removed topbar

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 /* appearance */
 static const unsigned int borderpx  = 7;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FiraCode Nerd Font:size=17:antialias=true:autohint=true:style=Bold" };
 static const char dmenufont[]       = "FiraCode Nerd Font:size=17:antialias=true:autohint=true:style=Bold";
