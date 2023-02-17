26e090e3ba09f2f33d19b4f2e7688fd61ed3b414
Amine Neggazi <neggazimedlamine@gmail.com>
2023-02-17 14:51:00 +0100

    updated all the colors

--- config.h
+++ config.h
@@ -7,15 +7,15 @@ static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=20" };
 static const char dmenufont[]       = "monospace:size=20";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_black[]       = "#222222";
+static const char col_gray1[]       = "#444444";
+static const char col_gray2[]       = "#bbbbbb";
+static const char col_gray3[]       = "#eeeeee";
+static const char col_ruby[]        = "#9b111e";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray2, col_black, col_gray1 },
+	[SchemeSel]  = { col_gray3, col_ruby,  col_ruby  },
 };
 
 /* tagging */
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray2, "-sb", col_ruby, "-sf", col_gray3, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
