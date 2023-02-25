5ed2d9860f754a6f65596bf1c4ba65ca2846bcc1
Amine Neggazi <neggazimedlamine@gmail.com>
2023-02-25 21:59:35 +0100

    switched back to monocle mode

--- config.h
+++ config.h
@@ -67,8 +67,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 #include "vanitygaps.c"
 static const Layout layouts[] = {
   /* symbol     arrange function */
-  // { "[M]",      monocle },
-  { "[@]",      spiral },
+  { "[M]",      monocle },
+  // { "[@]",      spiral },
   // { "[\\]",     dwindle },
   // { "H[]",      deck },
   // { "TTT",      bstack },
